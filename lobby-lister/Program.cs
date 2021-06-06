170166e36d4e130ae72a63302d70cbe8bbd9447e
Lucas Michaudel <38987429+Carbone13@users.noreply.github.com>
2021-06-06 20:25:24 +0200

    Initial Commit

---
+++ lobby-lister/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace lobby_lister
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
