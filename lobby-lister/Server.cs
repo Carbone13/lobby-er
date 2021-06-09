7b5bcf726b918fdf852c32c25ce78f046f05e03d
Lucas Michaudel <38987429+Carbone13@users.noreply.github.com>
2021-06-09 18:46:37 +0200

    putain de merde travailler avec vscode est une véritable infamie

--- lobby-lister/Server.cs
+++ lobby-lister/Server.cs
@@ -119,7 +119,7 @@ namespace LobbyEr
                 return;
             }
             
-            IPEndPoint hostEndpoint = IPEndPoint.Parse(hostEndpoint.ToString());
+            IPEndPoint hostEndpoint = IPEndPoint.Parse(lobbyHostPeer.Endpoint.ToString());
             IPEndPoint clientEndpoint = IPEndPoint.Parse(who.EndPoint.ToString());
 
             if(hostEndpoint.Address.ToString() == clientEndpoint.Address.ToString())
